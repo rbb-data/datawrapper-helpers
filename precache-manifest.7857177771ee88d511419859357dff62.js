ce716e4ece81ff44db116c96b2caec71c229b75f
Manuel Reich <mr@wundf.net>
2019-07-24 13:20:56 +0200

    Updates

---
+++ precache-manifest.7857177771ee88d511419859357dff62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3f9209c555966158c79a7e13ea9d42",
+    "url": "/datawrapper-helpers/index.html"
+  },
+  {
+    "revision": "ae466024846711b63e0b",
+    "url": "/datawrapper-helpers/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/datawrapper-helpers/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "ae466024846711b63e0b",
+    "url": "/datawrapper-helpers/static/js/main.008be408.chunk.js"
+  },
+  {
+    "revision": "ad8421de3fe858b840c8",
+    "url": "/datawrapper-helpers/static/js/runtime~main.d0da6904.js"
+  }
+]);
